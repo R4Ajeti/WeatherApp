b65f8654e9cd4c7e9c054b9a9b2bb7b6f6134ee2
Rinor Ajeti <r4ajeti@gmail.com>
2020-01-26 06:59:13 +0100

    ADDED: Finding weather by coords Finding weather by geographic coordinates latitude and longitude

--- src/weather/OpenWeatherMap.js
+++ src/weather/OpenWeatherMap.js
@@ -2,19 +2,50 @@ let APPID = '682251abec592051d124246aeedafbd4';
 let units = 'inperials';
 let searchMethod = '';
 const getSearchMethod = searchTerms => {
-  if (
-    searchTerms.length === 5 &&
-    Number.parseInt(searchTerms) + '' === searchTerms
-  )
-    searchMethod = 'zip';
-  else searchMethod = 'q';
-  return searchMethod;
+  let params = '';
+  let regex = '^[0-9]+.?[0-9]*,[0-9]+.?[0-9]*$';
+  let res = searchTerms.match(regex);
+  let getTag = tags =>
+    tags.indexOf(',') != -1 ? tags.substring(0, tags.indexOf(',')) : -1;
+  let tagsArr = [];
+  if (searchTerms.indexOf(',') == -1) {
+    params =
+      searchTerms.length === 5 &&
+      Number.parseInt(searchTerms) + '' === searchTerms
+        ? 'zip' + '=' + searchTerms
+        : 'q' + '=' + searchTerms;
+  } else {
+    while (searchTerms != '') {
+      let flag = false;
+      if (getTag(searchTerms) == -1) flag = true;
+      else tagsArr[tagsArr.length] = getTag(searchTerms);
+      if (flag) {
+        tagsArr[tagsArr.length] = searchTerms;
+        searchTerms = '';
+      } else {
+        searchTerms = searchTerms.substring(
+          tagsArr[tagsArr.length - 1].length + 1,
+          searchTerms.length
+        );
+      }
+      console.log(tagsArr);
+    }
+
+    params = 'lat=' + tagsArr[0] + '&lon=' + tagsArr[1];
+  }
+
+  return params;
 };
 function searchWeather(searchTerms) {
+  console.log(
+    `http://api.openweathermap.org/data/2.5/weather?${getSearchMethod(
+      searchTerms
+    )}&APPID=${APPID}&units=${units}`
+  );
   fetch(
     `http://api.openweathermap.org/data/2.5/weather?${getSearchMethod(
       searchTerms
-    )}=${searchTerms}&APPID=${APPID}&units=${units}`
+    )}&APPID=${APPID}&units=${units}`
   )
     .then(result => {
       return result.json();
